2fec23559885aa9735786cff50fef96e19a33b99
Dave Weernink <DavesRepository@users.noreply.github.com>
2019-01-15 22:49:54 +0100

    Lesson 146 loading data on startup

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -10,7 +10,23 @@ public class UnitOfMeasure {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long io;
+  private Long id;
 
-  private String uom;
+  private String description;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
 }
